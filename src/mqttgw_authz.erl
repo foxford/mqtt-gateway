2fd841482bab712bb3509309b33568b27c3f2ff8
Andrei Nesterov <ae.nesterov@gmail.com>
2019-02-21 11:36:06 -0800

    Add authorization config

--- src/mqttgw_authz.erl
+++ src/mqttgw_authz.erl
@@ -16,10 +16,51 @@
 %% =============================================================================
 
 -spec read_config(toml:config()) -> disabled | {enabled, config()}.
-read_config(_TomlConfig) ->
-    Config = #{},
-    {enabled, Config}.
+read_config(TomlConfig) ->
+    case toml:get_value(["features"], "authz", TomlConfig) of
+        {boolean, false} -> disabled;
+        _ ->
+            Config =
+                toml:folds(
+                    ["authn"],
+                    fun(_Config, Section, Acc) ->
+                        Aud = parse_audience(Section),
+                        Type = parse_type(Section, TomlConfig),
+                        Trusted = parse_trusted(Section, TomlConfig),
+                        Acc#{Aud => #{type => Type, trusted => Trusted}}
+                    end,
+                    #{},
+                    TomlConfig),
+
+            {enabled, Config}
+    end.
 
 -spec authorize(binary(), config()) -> boolean().
 authorize(_Audience, _Config) ->
     false.
+
+%% =============================================================================
+%% Internal functions
+%% =============================================================================
+
+-spec parse_audience(toml:section()) -> binary().
+parse_audience(["authz", Aud]) when is_list(Aud) ->
+    list_to_binary(Aud);
+parse_audience(Val) ->
+    error({bad_audience, Val}).
+
+-spec parse_type(toml:section(), toml:config()) -> trusted.
+parse_type(Section, Config) ->
+    case toml:get_value(Section, "type", Config) of
+        {string, <<"trusted">>} -> trusted;
+        none -> error(missing_type);
+        Other -> error({bad_type, Other})
+    end.
+
+-spec parse_trusted(toml:section(), toml:config()) -> gb_sets:set().
+parse_trusted(Section, Config) ->
+    case toml:get_value(Section, "trusted", Config) of
+        {array, {string, L}} -> gb_sets:from_list(lists:map(fun list_to_binary/1, L));
+        none -> error(missing_trusted);
+        Other -> error({bad_trusted, Other})
+    end.
