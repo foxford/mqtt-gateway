ed3667de476920637cf7a7796428d2bbbf15ff88
Andrei Nesterov <ae.nesterov@gmail.com>
2019-06-24 20:32:56 -0700

    Add support of dynamic subscription

---
+++ src/mqttgw_broker.erl
@@ -0,0 +1,40 @@
+-module(mqttgw_broker).
+
+%% API
+-export([
+    subscribe/2,
+    publish/3
+]).
+
+%% =============================================================================
+%% API
+%% =============================================================================
+
+%% Create a subscription for the particular client.
+%% We can verify the fact that subscription has been created by calling:
+%% vmq_subscriber_db:read({"", ClientId}).
+
+subscribe(ClientId, Topics) ->
+    wait_til_ready(),
+    CAPSubscribe = vmq_config:get_env(allow_subscribe_during_netsplit, false),
+    vmq_reg:subscribe(CAPSubscribe, {"", ClientId}, Topics),
+    ok.
+
+publish(Topic, Payload, QoS) ->
+    {_, Publish, _} = vmq_reg:direct_plugin_exports(mqttgw),
+    Publish(Topic, Payload, #{qos => QoS}),
+    ok.
+
+%% =============================================================================
+%% Internal functions
+%% =============================================================================
+
+-spec wait_til_ready() -> ok.
+wait_til_ready() ->
+    case catch vmq_cluster:if_ready(fun() -> true end, []) of
+        true ->
+            ok;
+        _ ->
+            timer:sleep(100),
+            wait_til_ready()
+end.
