8e93e23955c943edf41caf2214e345593fc60ffc
Shamir Khodzha <khodzha.sh@gmail.com>
2022-07-01 18:55:43 +0200

    added /api/v1/subscriptions POST route to create dynsubs (#92)

--- src/mqttgw_app.erl
+++ src/mqttgw_app.erl
@@ -15,6 +15,7 @@
 start(_StartType, _StartArgs) ->
     mqttgw_state:new(),
     mqttgw_ratelimitstate:new(),
+    ok = mqttgw_http:start(mqttgw_authn:read_config()),
     mqttgw_sup:start_link().
 
 stop(_State) ->
